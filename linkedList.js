0085fffa8e871ea1b5505cc4fa23408dd05fcbca
Mattdre <olsen_matthew@yahoo.com>
2016-06-13 12:29:33 -1000

    passed all 21 tests

--- linkedList.js
+++ linkedList.js
@@ -7,7 +7,6 @@ function linkedListGenerator(){
     var head = null;
     var tail = null;
     var node = {};
-    var nodeIndex = 0;
 
   function _getHead(){
     return head;
@@ -19,7 +18,6 @@ function linkedListGenerator(){
 
   function _add(value){
     node = {
-      nodeIndex: nodeIndex++,
       value: value,
       next: null
     };
@@ -36,23 +34,47 @@ function linkedListGenerator(){
   function _get(number){
     var thisNode = head;
     for (var i = 0; i < number; i++) {
-      thisNode = thisNode.next;
-      if(thisNode === null){
+      if(thisNode.next === null){
         return false;
       }
+      thisNode = thisNode.next;
     }
     return thisNode;
   }
 
   function _remove(number){
-    var thisNode = head;
-    for (var i = 0; i < number; i++) {
-      thisNode = thisNode.next;
+    var currentNode = _get(number);
+    var prevNode = _get(number - 1);
+    var nextNode = _get(number + 1);
+
+    if(currentNode === head){
+      head = nextNode;
+    }else if(currentNode.next === null){
+      prevNode.next = null;
+      tail = prevNode;
+    }else if(currentNode === false){
+      return false;
     }
+    prevNode.next = nextNode;
   }
 
-  function _insert(Value, Number){
-    if (true) {}
+  function _insert(value, number){
+    var newNode = {
+      value: value,
+      next: null
+    };
+    var desiredPlace = _get(number);
+    var prevNode = _get(number-1);
+
+    if(desiredPlace === false || number < 0){
+      return false;
+    }else if(number === 0){
+      head = newNode;
+      newNode.next = desiredPlace;
+    }else if(desiredPlace !== head || desiredPlace !== tail){
+      newNode.next = desiredPlace;
+      prevNode.next = newNode;
+    }
   }
 
   return {
